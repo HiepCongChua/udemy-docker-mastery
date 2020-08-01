ce66c36761d8d240c14940c1c2a09de6c947cdf4
HiepCongChua <hieplevuc@gmail.com>
2020-08-01 09:02:58 +0700

    update api

--- docker-node/src/config/main.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    port : process.env.PORT || 3000,
-    mongoURL :  process.env.MONGODB_URL || 'mongodb://localhost:27018/example',
-    env:process.env.NODE_ENV || 'development'
-}
